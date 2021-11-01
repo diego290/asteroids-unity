43aa4e9f3e06236c0f6cad03e2563b2afa206c28
Diego <diegorodrigues290@hotmail.com>
2021-11-01 14:11:59 -0300

    inicio

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public ParticleSystem explosionEffect;
+    public GameObject gameOverUI;
+
+    public int score { get; private set; }
+    public Text scoreText;
+
+    public int lives { get; private set; }
+    public Text livesText;
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (this.lives <= 0 && Input.GetKeyDown(KeyCode.Return)) {
+            NewGame();
+        }
+    }
+
+    public void NewGame()
+    {
+        Asteroid[] asteroids = FindObjectsOfType<Asteroid>();
+
+        for (int i = 0; i < asteroids.Length; i++) {
+            Destroy(asteroids[i].gameObject);
+        }
+
+        this.gameOverUI.SetActive(false);
+
+        SetScore(0);
+        SetLives(3);
+        Respawn();
+    }
+
+    public void Respawn()
+    {
+        this.player.transform.position = Vector3.zero;
+        this.player.gameObject.SetActive(true);
+    }
+
+    public void AsteroidDestroyed(Asteroid asteroid)
+    {
+        this.explosionEffect.transform.position = asteroid.transform.position;
+        this.explosionEffect.Play();
+
+        if (asteroid.size < 0.7f) {
+            SetScore(this.score + 100); // small asteroid
+        } else if (asteroid.size < 1.4f) {
+            SetScore(this.score + 50); // medium asteroid
+        } else {
+            SetScore(this.score + 25); // large asteroid
+        }
+    }
+
+    public void PlayerDeath(Player player)
+    {
+        this.explosionEffect.transform.position = player.transform.position;
+        this.explosionEffect.Play();
+
+        SetLives(this.lives - 1);
+
+        if (this.lives <= 0) {
+            GameOver();
+        } else {
+            Invoke(nameof(Respawn), player.respawnDelay);
+        }
+    }
+
+    public void GameOver()
+    {
+        this.gameOverUI.SetActive(true);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        this.scoreText.text = score.ToString();
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+        this.livesText.text = lives.ToString();
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public Bullet bulletPrefab;
+
+    public float thrustSpeed = 1.0f;
+    public bool thrusting { get; private set; }
+
+    public float turnDirection { get; private set; } = 0.0f;
+    public float rotationSpeed = 0.1f;
+
+    public float respawnDelay = 3.0f;
+    public float respawnInvulnerability = 3.0f;
+
+    private void Awake()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        // Turn off collisions for a few seconds after spawning to ensure the
+        // player has enough time to safely move away from asteroids
+        this.gameObject.layer = LayerMask.NameToLayer("Ignore Collisions");
+        Invoke(nameof(TurnOnCollisions), this.respawnInvulnerability);
+    }
+
+    private void Update()
+    {
+        this.thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            this.turnDirection = 1.0f;
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            this.turnDirection = -1.0f;
+        } else {
+            this.turnDirection = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.thrusting) {
+            this.rigidbody.AddForce(this.transform.up * this.thrustSpeed);
+        }
+
+        if (this.turnDirection != 0.0f) {
+            this.rigidbody.AddTorque(this.rotationSpeed * this.turnDirection);
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+        bullet.Project(this.transform.up);
+    }
+
+    private void TurnOnCollisions()
+    {
+        this.gameObject.layer = LayerMask.NameToLayer("Player");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Asteroid")
+        {
+            this.rigidbody.velocity = Vector3.zero;
+            this.rigidbody.angularVelocity = 0.0f;
+            this.gameObject.SetActive(false);
+
+            FindObjectOfType<GameManager>().PlayerDeath(this);
+        }
+    }
+
+}
